eb75b55a6b4f8d5681881a06ad16e14348c6fc7d
Mohanad Kadadou <muhanndkadadou0@gmail.com>
2026-03-21 22:10:16 +0300

    remove pino-http to fix build

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,8 @@
 import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
 import router from "./routes";
-import { logger } from "./lib/logger";
 
 const app: Express = express();
-
-// Logger middleware
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req: Request) {
-        return {
-          method: req.method,
-          url: req.url,
-        };
-      },
-      res(res: Response) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  })
-);
 
 // Middlewares
 app.use(cors());
@@ -34,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api", router);
 
-// Test route (مهم للتأكد أن السيرفر يعمل)
+// Test route
 app.get("/", (req: Request, res: Response) => {
   res.send("API is running 🚀");
 });
